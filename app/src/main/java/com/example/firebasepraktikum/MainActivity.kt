8c4e9a670d43b881197101320c2a7f688f60cc10
Viaasyafrina16 <syafrinavia@gmail.com>
2026-01-02 17:35:41 +0700

    memanggil DataSiswaApp untuk menjalankan aplikasi

--- app/src/main/java/com/example/firebasepraktikum/MainActivity.kt
+++ app/src/main/java/com/example/firebasepraktikum/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.firebasepraktikum.ui.theme.FirebasePraktikumTheme
+import com.example.firebasepraktikum.view.controlNavigasi.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             FirebasePraktikumTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
+
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirebasePraktikumTheme {
-        Greeting("Android")
     }
 }
