7981c6dd6ff02d7c4a0cbcb7e50931c7317a0bb5
Viaasyafrina16 <syafrinavia@gmail.com>
2026-01-02 17:47:06 +0700

    Menambahkan DestinasiEdit untuk navigasi halaman edit siswa dengan parameter ID

---
+++ app/src/main/java/com/example/firebasepraktikum/view/Route/DestinasiEdit.kt
@@ -0,0 +1,10 @@
+package com.example.firebasepraktikum.view.Route
+
+import com.example.firebasepraktikum.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
